1042496f3a2fed05aa9550268ddbe022070ad701
JAY\xenon <dlaworud@koreatech.ac.kr>
2023-06-18 13:17:24 +0900

    백준 1389번: 케빈 베이컨의 6단계 법칙

--- Class3/1389.py
+++ Class3/1389.py
@@ -1,43 +1,37 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-def dfs(count, start, end):
-    visited[u] = True
-    count += 1
-    for s in network[start]:
-        if visited[end]:
-            return count
-        if not visited[end]:
-            print(count, s, end)
-            print(visited[end])
-            dfs(count, s, end)
+def bfs(start):
+    count = [0 for _ in range(n+1)]
+    visited = [start]
+    queue = deque()
+    queue.append(start)
+    while queue:
+        vertices = queue.popleft()
+        for v in graph[vertices]:
+            if v not in visited:
+                count[v] += count[vertices] + 1
+                visited.append(v)
+                queue.append(v)
+    return sum(count)
 
 n, m = map(int, input().split())
+graph = {}
 
-network = {}
-visited = [False for _ in range(n)]
-res = 9999999
-
-# 입력
 for _ in range(m):
     u, v = map(int, input().split())
-    if u in network:
-        network[u].append(v)
+    if u in graph:
+        graph[u].append(v)
     else:
-        network[u] = [v]
-    if v in network:
-        network[v].append(u)
+        graph[u] = [v]
+    if v in graph:
+        graph[v].append(u)
     else:
-        network[v] = [u]
+        graph[v] = [u]
 
-print(network)
+res = []
+for i in range(1, n+1):
+    res.append(bfs(i))
 
-for start in range(1, n+1):
-    sum = 0
-    for end in range(1, n+1):
-        count = 0
-        sum += dfs(count, start, end)
-    if sum < res:
-        res = sum
-        
-print(res)
+print(res.index(min(res))+1)
